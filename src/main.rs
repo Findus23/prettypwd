2010101b98a9b444eb01389f8dfdfd41587720f5
Lukas Winkler <git@lw1.at>
2022-02-05 21:39:23 +0100

    format code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate core;
 
 use std::env;
 
-
 fn simplify_string(pwd: String, home: String) -> String {
     let prettyhome = pwd.replacen(home.as_str(), "/~", 1);
     let dirs: Vec<&str> = prettyhome.split("/").collect();
@@ -14,7 +13,9 @@ fn simplify_string(pwd: String, home: String) -> String {
         print_dirs.push(dirs[1]);
         print_dirs.push("…");
         print_dirs.extend(dirs[dirs.len() - 2..].to_vec())
-    } else { print_dirs = dirs }
+    } else {
+        print_dirs = dirs
+    }
     // println!("{:#?}", print_dirs);
     return print_dirs.join("/").replacen("/~", "~", 1);
 }
@@ -23,16 +24,15 @@ fn main() {
     let pwd = env::var("PWD");
     let pwd = match pwd {
         Ok(test) => test,
-        Err(error) => panic!("{:?}", error)
+        Err(error) => panic!("{:?}", error),
     };
     let home = env::var("HOME");
     let home = match home {
         Ok(test) => test,
-        Err(error) => panic!("{:?}", error)
+        Err(error) => panic!("{:?}", error),
     };
     println!("{}", simplify_string(pwd, home));
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -42,8 +42,8 @@ mod tests {
 
     fn comparison_helper(input: &str, output: &str) {
         assert_eq!(
-            simplify_string(input.to_string(), TEST_HOME.to_string())
-            , output.to_string()
+            simplify_string(input.to_string(), TEST_HOME.to_string()),
+            output.to_string()
         );
     }
 
@@ -61,6 +61,9 @@ mod tests {
     #[test]
     fn homedir() {
         comparison_helper("/home/user/sometool/subdir", "~/sometool/subdir");
-        comparison_helper("/home/user/sometool/subdir/subsubdir", "~/…/subdir/subsubdir");
+        comparison_helper(
+            "/home/user/sometool/subdir/subsubdir",
+            "~/…/subdir/subsubdir",
+        );
     }
 }
